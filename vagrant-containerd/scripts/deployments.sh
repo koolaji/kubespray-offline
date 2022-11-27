58e85563c8466b5ed08238137558c0dcb9ff8d7b
root <root@devops-test2.novalocal>
2022-11-27 16:03:33 +0000

    final-0.0.1

--- vagrant-containerd/scripts/deployments.sh
+++ vagrant-containerd/scripts/deployments.sh
@@ -1,12 +1,13 @@
-if [ $1 = 'Nmaster' ]
+if [ $1 = 'master' ]
 then
+	set -x
         export DEBIAN_FRONTEND=noninteractive
         echo 'nameserver 8.8.8.8' > /etc/resolv.conf
         ln -sf /usr/share/zoneinfo/Asia/Tehran /etc/localtime
-        apt install -y ntp sshpass
+	apt update &&   apt install -y ntp sshpass
         ntpq -p
 	cp /vagrant/configs/hosts /etc/hosts
-	cp -r /tmp/outputs /home/vagrant && rm -r /tmp/outputs
+	cp -r /tmp/outputs /root && rm -r /tmp/outputs
 	ssh-keygen -t rsa -b 4096 -f ~/.ssh/id_rsa -N ""
 	cd /root/outputs
 	bash setup-container.sh
@@ -22,9 +23,13 @@ then
 	pip install -U pip
 	pip install -r requirements.txt
 	cp -r  ../playbook/* .
-	sshpass -f 123456 ssh-copy-id vagrant@node01
-	sshpass -f 123456 ssh-copy-id vagrant@node02
-	sshpass -f 123456 ssh-copy-id vagrant@node03
+	sshpass -p "vagrant" ssh-copy-id -o StrictHostKeyChecking=no root@node01
+	sshpass -p "vagrant" ssh-copy-id -o StrictHostKeyChecking=no root@node02
+	sshpass -p "vagrant" ssh-copy-id -o StrictHostKeyChecking=no root@node03
+	sshpass -p "vagrant" ssh-copy-id -o StrictHostKeyChecking=no root@node04
+	cp -rfp inventory/sample inventory/mycluster
+	cp  /vagrant/configs/offline.yml  inventory/mycluster/group_vars/all/offline.yml
+	cp  /vagrant/configs/inventory.ini     inventory/mycluster
 	ansible-playbook -i inventory/mycluster/inventory.ini offline-repo.yml
 	ansible-playbook -i inventory/mycluster/inventory.ini  --become --become-user=root cluster.yml
 else
@@ -32,7 +37,10 @@ else
         echo 'nameserver 8.8.8.8' > /etc/resolv.conf
 	cp /vagrant//configs/hosts /etc/hosts
         ln -sf /usr/share/zoneinfo/Asia/Tehran /etc/localtime
-        apt install -y  ntp
+	apt update &&   apt install -y ntp sshpass
         ntpq -p
+	echo -e  "vagrant\nvagrant" | passwd
+	cp /vagrant/configs/sshd_config /etc/ssh/sshd_config
+	systemctl restart ssh
 fi
 
